d55e2300d263f48b2122da316b7cf82072acaeed
sksjw95 <tjwldnd95@hanmail.net>
2023-11-06 10:32:25 +0900

    ex3

---
+++ c++연습 과제 상관없음/ex3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	char ch;
+	if (ch == 'e') {
+		cout << "동쪽으로 이동" << endl;
+	}
+	else if (ch == 'w') {
+		cout << "서쪽으로 이동" << endl;
+	}
+	else if (ch == 's') {
+		cout << "남쪽으로 이동" << endl;
+	}
+	else if (ch == 'n') {
+		cout << "북쪽으로 이동" << endl;
+	}
+}
