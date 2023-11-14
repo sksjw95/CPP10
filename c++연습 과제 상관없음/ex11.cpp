5a0100e7f478bb22e55560369853d215c5ff6d75
sksjw95 <tjwldnd95@hanmail.net>
2023-11-15 00:32:00 +0900

    ex11

---
+++ c++연습 과제 상관없음/ex11.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int MaxNum(const int arr[], int n) {
+	int Max = arr[0];
+	for (int i = 1; i < n; i++) {
+		if (Max < arr[i]) {
+			Max = arr[i];
+		}
+	}
+
+	return Max;
+}
+
+float MaxNum(const float arr[], int n) {
+	float Max = arr[0];
+	for (int i = 1; i < n; i++) {
+		if (Max < arr[i]) {
+			Max = arr[i];
+		}
+	}
+
+	return Max;
+}
+
+
+
+int main() {
+	int arr[5] = { 1,4,7,8,2 };
+	cout << MaxNum(arr, 5) << endl;
+}
