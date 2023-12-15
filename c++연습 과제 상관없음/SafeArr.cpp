48ef4d71d80e48b7deb0f56264a731991c77aa13
sksjw95 <tjwldnd95@hanmail.net>
2023-12-16 01:16:10 +0900

    SafeArr

---
+++ c++연습 과제 상관없음/SafeArr.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include "SafeIntArray.h"
+using namespace std;
+
+int main() {
+	SafeIntArray a(10);
+
+	for (int i = 0; i < 10; i++) {
+		a[i] = i;
+	}
+	cout << a[5] << endl;
+	cout << a[12] << endl;
+
+	return 0;
+}
